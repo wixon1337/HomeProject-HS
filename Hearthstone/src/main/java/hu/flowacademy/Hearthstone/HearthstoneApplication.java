7f7ecc0d62283212a79195107f01442b82da7c91
Vilmos Pásztor <pvilmos93@gmail.com>
2019-09-11 16:47:07 +0200

    project generated

---
+++ Hearthstone/src/main/java/hu/flowacademy/Hearthstone/HearthstoneApplication.java
@@ -0,0 +1,13 @@
+package hu.flowacademy.Hearthstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearthstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearthstoneApplication.class, args);
+	}
+
+}
